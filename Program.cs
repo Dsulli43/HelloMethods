b075030cdcfc6e42108409eaabc2b03868436656
Dsulli43 <126179726+Dsulli43@users.noreply.github.com>
2023-03-02 10:46:55 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+namespace HelloMethods
+{
+    public class program
+    {
+
+    
+        public static void Main(string[] args)
+        {
+            string message = Message.GetMessage("la");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+    
+}
+
